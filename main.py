7fca7da028b3b4a8ea943fc487ee3fb595d945ea
Patataman <shas_arroyo@hotmail.com>
2017-03-16 15:45:53 +0100

    carpeta templates y cosas main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from models.connection import db
 
 # Inicializacion del objeto Flask
@@ -15,6 +15,12 @@ db.init_app(app)
 @app.route('/')
 def index():
     return 'Hola mundo'
+
+@app.route('/prestamo/create', methods=['GET','POST'])
+def loan_create():
+	if request.method == 'POST':
+		return render_template("index.html")
+	return render_template("loan_create.html")
 
 if __name__ == '__main__':
     app.run()

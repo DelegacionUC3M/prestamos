8382d3e174c7e50addce93a693196ff33ab9c440
servinagrero <servinagrero@gmail.com>
2018-07-01 23:46:32 +0200

    Updated templates and styles

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ two_time_objects = ['electronico', 'electrico']
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("index.html",

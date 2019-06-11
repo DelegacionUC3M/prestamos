6ea2f125f3ff99a25318c15b92a48914045bac5d
servinagrero <servinagrero@gmail.com>
2019-06-11 22:48:19 +0200

    Updated models

--- models/models.go
+++ models/models.go
@@ -2,25 +2,28 @@ package models
 
 import (
 	"time"
+	"github.com/jinzhu/gorm"
 )
 
 // Item stores the information from a lendable object
 type Item struct {
-	ID                 uint `gorm:"primary_key" gorm:"AUTO_INCREMENT"`
-	Name               string
-	Amount             int
-	Type               string
-	State              string
-	LoanDays           int
-	PenaltyCoefficient float32
+	// ID                 uint    `gorm:"primary_key";"AUTO_INCREMENT"`
+	gorm.Model
+	Name               string  `gorm:"not_null"`
+	Amount             int	   `gorm:"not_null"`
+	Type               string  `gorm:"not_null"`
+	State              string  `gorm:"not_null"`
+	LoanDays           int	   `gorm:"not_null"`
+	PenaltyCoefficient float64 `gorm:"not_null"`
 }
 
 // Loan stores a loan made from a user
 type Loan struct {
-	ID         uint `gorm:"primary_key" gorm:"AUTO_INCREMENT"`
+	// ID         uint `gorm:"primary_key";"AUTO_INCREMENT"`
+	gorm.Model
 	Item       Item `gorm:"foreignkey:ItemID"`
 	ItemID     int 
-	Nia        int 'gorm:"not null"'
+	Nia        int  `gorm:"not null"`
 	Amount     int
 	LoanDate   time.Time
 	RefundDate time.Time
@@ -28,7 +31,8 @@ type Loan struct {
 
 // Penalty stores a sanction from a user
 type Penalty struct {
-	ID           uint `gorm:"primary_key" gorm:"AUTO_INCREMENT"`
+	// ID           uint `gorm:"primary_key";"AUTO_INCREMENT"`
+	gorm.Model
 	Nia          int
 	Loan         Loan `gorm:"foreignkey:LoanID"`
 	LoanID       int
